b5670977763319ee9d82794b66cca319b5a7aab1
Aarya Upadhyay <aarya7u@gmail.com>
2026-04-08 16:55:01 +0530

    Dark Mode added

--- script.js
+++ script.js
@@ -134,3 +134,16 @@ sortSelect.addEventListener("change", function() {
 
   displayCountries(sortedData);
 });
+
+// DARK MODE
+
+let toggleBtn = document.getElementById("toggleTheme")
+toggleBtn.addEventListener("click",function(){
+  document.body.classList.toggle("dark")
+
+  if(document.body.classList.contains("dark")){
+    toggleBtn.innerText="☀️ Light Mode";
+  }else{
+    toggleBtn.innerText="🌙 Dark Mode"
+  }
+})
